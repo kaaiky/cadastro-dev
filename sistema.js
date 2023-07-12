9a338bc527bd7a909ec00ba3a86364f9a50c2b9f
kaaiky <kaiky.s@gsuite.iff.edu.br>
2023-07-12 17:11:12 -0300

    git commit

--- sistema.js
+++ sistema.js
@@ -2,7 +2,7 @@ const { error } = require('console');
 const express = require('express');
 const app = express()
 const fs = require("fs")
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 app.set('views', './views')
 app.use(express.static("public"));
@@ -11,51 +11,24 @@ app.get('/', (req, res) => {
 });
 app.post('/', (req, res) => {
     const dados  = JSON.stringify(req.body)
-    const nomeArq = req.body.email+".txt"
+    const nomeArq = req.body.email+".Json"
     fs.writeFileSync(nomeArq, dados)
-    console.log({valor: nomeArq});
     res.render('sucesso');
-});
-app.get('/lista',(req,res)=>{
-  const rotaContasFeitas = app.route("/views");
-  rotaContasFeitas.get((req, res) => {
-      let contasGravadas = [];
-      arquivos.readdirSync("./dados/").forEach(file => {
-        if (file.includes(".txt")) {
-          contasGravadas.push(file);
-        }
-      });
-      console.log(contasGravadas);
-      res.render("lista", {contas: contasGravadas});
-  })  
-})
-  app.get('/:email/show', (req,res)=>{
-    var dados = req.params
-    var dadosfinais = JSON.parse(fs.readFileSync(dados.email))
-    res.render('dadosfinais', {dadosfinais: JSON.stringify(dadosfinais)})
-    })
-
-/*
-const rotaContasFeitas = app.route("/views");
-rotaContasFeitas.get((req, res) => {
-    let contasGravadas = [];
-    arquivos.readdirSync("./dados/").forEach(file => {
-      if (file.includes(".txt")) {
-        contasGravadas.push(file);
-      }
-    });
-    console.log(contasGravadas);
-    res.render("lista", {contas: contasGravadas});
 })
 
-const rotaUmaConta = app.route("/umaconta");
-rotaUmaConta.get((req, res) => {
-  const nomeArq = "dados/"+req.query.nome
-  const conteudoArq = arquivos.readFileSync(nomeArq)
-  const stringArq = conteudoArq.toString()
-  const objArq = JSON.parse(stringArq)
-  res.render("umaconta", {conta: objArq});
-})*/
+app.post('/lista', (req,res)=>{
+  fs.readdir(__dirname, (err, files)=>{
+     if (err) console.log(err);
+     var newfiles =  files.filter(data => data.includes('.Json') )
+     console.log(newfiles);
+      res.render('lista', {files: newfiles})
+  })
+})
+app.get('/delete/:email', (req,res)=>{
+    var dados = req.params.email
+    fs.unlinkSync(dados)
+    res.send("dados apagados!")
+})
 
 app.listen(8080)
 console.log("servidor rodando...")
