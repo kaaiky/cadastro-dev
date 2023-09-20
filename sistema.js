222a44ee84ef0e7cdcdcd7ffaeedefe488f6965a
kaaiky <kaiky.s@gsuite.iff.edu.br>
2023-09-20 08:17:56 -0300

    commit-two

--- sistema.js
+++ sistema.js
@@ -1,34 +1,34 @@
-const { error } = require('console');
 const express = require('express');
 const app = express()
+
 const fs = require("fs")
-app.use(express.urlencoded({extended: true}))
-app.set('view engine', 'ejs');
-app.set('views', './views')
-app.use(express.static("public"));
-app.get('/', (req, res) => {
-    res.render('cadastro');
-});
-app.post('/', (req, res) => {
+app.use(express.json())
+
+
+app.post('/post', (req, res) => {
     const dados  = JSON.stringify(req.body)
-    const nomeArq = req.body.email+".Json"
+    const nomeArq = req.body.email+".json"
     fs.writeFileSync(nomeArq, dados)
-    res.render('sucesso');
+    res.send('sucesso');
 })
 
-app.post('/lista', (req,res)=>{
+app.get('/lista', (req,res)=>{
   fs.readdir(__dirname, (err, files)=>{
      if (err) console.log(err);
-     var newfiles =  files.filter(data => data.includes('.Json') )
+     var newfiles =  files.filter(data => data.includes('.json') )
      console.log(newfiles);
-      res.render('lista', {files: newfiles})
+      res.send({files: newfiles})
   })
 })
-app.get('/delete/:email', (req,res)=>{
+app.delete('/delete/:email', (req,res)=>{
     var dados = req.params.email
     fs.unlinkSync(dados)
     res.send("dados apagados!")
 })
+
+app.use((req, res, next) => {
+    res.send({erro: true, msg: "Rota não definida no servidor."})
+});
 
 app.listen(8080)
 console.log("servidor rodando...")
